41eee6794e82024ed12ca3c781324b549b8b9d77
Trevor Nelson <tsnelson86@gmail.com>
2017-08-20 17:41:58 -0600

    syncing directory

---
+++ assets/timer.js
@@ -0,0 +1,82 @@
+var questions = {
+	question1 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question2 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question3 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question4 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question5 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question6 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question7 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question8 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	},
+	question9 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	}
+	question10 : {
+		question: 'ewok',
+		guess1:"An Ewok", 
+		guess2: 400, 
+		guess3: 5,
+		guess4: 5,
+		answer: 5
+	}
+};
